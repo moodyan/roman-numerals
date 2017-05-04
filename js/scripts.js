25fc34b38b7b3a0e2aee5fe98e7a55cec9a8b226
Alyssa Moody <alyssanicholemoody@gmail.com>
2017-05-04 10:19:59 -0700

    Add for loop that handles all number conversions.

--- js/scripts.js
+++ js/scripts.js
@@ -5,21 +5,20 @@ function toRomanNumerals(num){
   var numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
   var roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
 
-  if (!+num) {
-    return false;
-  }
-
-  if (num > 3999) {
-    alert("Number is too big!");
+  if (num<=0 || num>3999) {
+    alert("Number cannot be 0 or a negative number and cannot be larger than 3,999. Please enter a different number.");
     return false;
   }
 
   for (var i = 0; i < numbers.length; i++) {
-
+    while (num%numbers[i] < num) {
+      result += roman[i];
+      num -= numbers[i];
+    }
   }
 
 
- return(result);
+ return result;
 }
 
 
