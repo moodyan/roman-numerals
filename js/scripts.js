7be2331240690d9d3fa8594300ad138b8b1cdb5f
Alyssa Moody <alyssanicholemoody@gmail.com>
2017-05-04 09:47:55 -0700

    Set variables for numbers and roman numerals.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,25 @@
 //back-end
 
 function toRomanNumerals(num){
-  var result = "";
-  num = parseInt(num);
+  var result = [];
+  var numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+
   if (!+num) {
     return false;
   }
+
   if (num > 3999) {
     alert("Number is too big!");
     return false;
   }
 
+  for (var i = 0; i < numbers.length; i++) {
 
- console.log(roman);
+  }
+
+
+ return(result);
 }
 
 
@@ -24,7 +31,7 @@ $(document).ready(function(){
 
     var num = $("#number").val()
 
-    var roman = toRomanNumerals(num)
-    $("ul").append("<li>"+roman+"</li>")
+    var result = toRomanNumerals(num)
+    $("ul").append("<li>"+result+"</li>")
   });
 });
